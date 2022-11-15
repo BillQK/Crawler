812f7c6789d7f738fecb00cc368f4d9e500db229
justagoodfriend <greenate1@gmail.com>
2022-11-14 20:38:06 -0500

    abstracted log in with too many comments

--- htmlparser.py
+++ htmlparser.py
@@ -16,17 +16,4 @@ class MyHTMLParser(HTMLParser):
         print("encountered data :" , data)
 
 parser = MyHTMLParser()
-#parser.feed('<html><head><title>Test</title></head>'
-#            '<body><input type ="hidden" name = crsfmiddlewaretoken" value = dlkafjadkljfl><input type = "submit" value="Log in"></body></html>')
-
-teststring = "Date: Fri 11 Nov 2022 Content-Type: text/html klafjadkldj Set-Cookie: csrftoken=dja2642632klfjakldsjfklas sessionid=dkalfjadkljflka"
-testlist = teststring.split()
-for i in testlist:
-    if "csrftoken" in i or "sessionid" in i:
-        print(i)       
-#pattern = re.compile(r'csrftoken=.*')
-
-
-#pattern2 = re.compile(r'sessionid=.+')
-#matches = re.findall(pattern, testlist)
-#print(matches)
+parser.feed('<li><a href="/fakebook/9991121/">Edmundo Gresl</a></li>')
